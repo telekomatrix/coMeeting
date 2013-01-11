03e9113c168f1894e10c7a73e820d83cec296df6
Guilherme Simoes <guilherme.rdems@gmail.com>
2013-01-11 01:53:38 +0000

    Now sending invite emails.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
     I18n.locale = params[:locale] || I18n.default_locale
   end
 
-  # ensure locale persists
+  # Ensure locale persists
   def default_url_options(options={})
     {:locale => I18n.locale == I18n.default_locale ? nil : I18n.locale}
   end
--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -4,17 +4,13 @@ class EmailsController < ApplicationController
     if sender.nil?
       flash.now[:error] = t("email.controller.reinvite.error.notfound.sender")
     else
-
       recipient = sender.meeting.participations.where(:user_id => params[:id]).first
       if recipient.nil?
         flash.now[:error] = t("email.controller.reinvite.error.notfound.recipient")
       else
-
-        #UserMailer.invite(sender.user.name_formatted, recipient).deliver
+        UserMailer.invite(sender.user.name_formatted, recipient).deliver
         flash.now[:notice] = t("email.controller.reinvite.notice")
-
       end
-
     end
     render 'shared/flash_messages'
   end
